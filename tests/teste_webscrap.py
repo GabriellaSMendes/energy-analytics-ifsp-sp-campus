e012d67be29dbaa3f415bbf5ec41eb9f06774ac7
GabriellaIFSP <gabriella.smendes@outlook.com>
2026-01-22 18:00:27 -0300

    Testes acessando SUAP - login

---
+++ tests/teste_webscrap.py
@@ -0,0 +1,50 @@
+#webdriver: é o controle da tela no navegador. Responsável por cliques, navegações, etc.  
+#service: dentro do webdriver, é quem fala direta e especificamente com o Google Chrome  
+#By: responsável pelas buscas no HTML
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+#Padrão de inicialização. Quando formos rodar na máquina do IFSP teremos que, provavelmente, acrescentar alguns parâmetros nessas duas funções (service e options)
+service = Service()
+options = webdriver.ChromeOptions()
+
+# Aqui o ChromeDriver é iniciado, retorna esse objeto "driver" para controle do navegador
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.maximize_window() #maximiza a tela
+
+url = 'https://suap.ifsp.edu.br/'
+driver.get(url)
+
+wait = WebDriverWait(driver, 20)
+
+
+# TELA DE LOGIN
+#preencher matrícula 
+username_field = wait.until(
+     EC.element_to_be_clickable((By.XPATH, '//input[@id="id_username"]'))
+)
+
+username_field.clear()
+username_field.send_keys("mudar-para-sua-matricula") #MUDAR PARA RODAR
+
+#preencher senha
+password_field = wait.until(
+     EC.element_to_be_clickable((By.XPATH, '//input[@id="id_password"]'))
+)
+
+password_field.clear()
+password_field.send_keys("mudar-para-sua-senha") #MUDAR PARA RODAR
+
+#botão enviar
+send_login = driver.find_element(By.XPATH, '//input[@type="submit" and @value="Acessar"]')
+
+send_login.click()
+
+#Encerra o navegador
+input("Pressione Enter para fechar...")
+driver.quit()
