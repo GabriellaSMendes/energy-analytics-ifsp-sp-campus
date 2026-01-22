0b56e3e1f037ec643920b9fcb664983f59ab5e4e
GabriellaIFSP <gabriella.smendes@outlook.com>
2026-01-22 19:17:14 -0300

    Extraindo documento do SUAP

--- tests/teste_webscrap.py
+++ tests/teste_webscrap.py
@@ -7,10 +7,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-#Padrão de inicialização. Quando formos rodar na máquina do IFSP teremos que, provavelmente, acrescentar alguns parâmetros nessas duas funções (service e options)
+#Padrão de inicialização
 service = Service()
 options = webdriver.ChromeOptions()
+options.add_experimental_option(
+     "prefs",
+     {"plugins.always_open_pdf_externally": True}
+)
 
 # Aqui o ChromeDriver é iniciado, retorna esse objeto "driver" para controle do navegador
 driver = webdriver.Chrome(service=service, options=options)
@@ -22,28 +25,51 @@ driver.get(url)
 
 wait = WebDriverWait(driver, 20)
 
+#função para o clique
+def click(xpath):
+     element = wait.until(
+          EC.element_to_be_clickable((By.XPATH, xpath))
+     )
+     element.click()
 
-# TELA DE LOGIN
-#preencher matrícula 
-username_field = wait.until(
-     EC.element_to_be_clickable((By.XPATH, '//input[@id="id_username"]'))
-)
+def login ():
+     username = "mudar-para-sua-matricula" #MUDAR PARA RODAR
+     password = "mudar-para-sua-senha" #MUDAR PARA RODAR
+     
+     #preencher matrícula 
+     username_field = wait.until(
+          EC.element_to_be_clickable((By.XPATH, '//input[@id="id_username"]'))
+     )
 
-username_field.clear()
-username_field.send_keys("mudar-para-sua-matricula") #MUDAR PARA RODAR
+     username_field.clear()
+     username_field.send_keys(username) 
 
-#preencher senha
-password_field = wait.until(
-     EC.element_to_be_clickable((By.XPATH, '//input[@id="id_password"]'))
-)
+     #preencher senha
+     password_field = wait.until(
+          EC.element_to_be_clickable((By.XPATH, '//input[@id="id_password"]'))
+     )
 
-password_field.clear()
-password_field.send_keys("mudar-para-sua-senha") #MUDAR PARA RODAR
+     password_field.clear()
+     password_field.send_keys(password) 
 
-#botão enviar
-send_login = driver.find_element(By.XPATH, '//input[@type="submit" and @value="Acessar"]')
+     #botão enviar
+     click('//input[@type="submit" and @value="Acessar"]')
 
-send_login.click()
+login()
+
+#Abrindo o perfil
+click('//div[contains(@class, "photo-circle")]')
+
+#Abrindo dropdown de documentos
+click('//a[contains(@class, "default")]')
+
+#Selecionando Carteirinha
+click('//a[normalize-space()="Comprovante de Dados Acadêmicos"]')
+
+     
+     
+     
+     
 
 #Encerra o navegador
 input("Pressione Enter para fechar...")
